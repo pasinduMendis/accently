2480eb6542ba9dafcaf3e6427d1f1306fa8c574f
Pasindu Mendis <86598324+pasinduMendis@users.noreply.github.com>
2022-11-22 20:23:34 +0530

    Update googleAndApple-pay.js

--- client/js/googleAndApple-pay.js
+++ client/js/googleAndApple-pay.js
@@ -110,10 +110,13 @@ document.addEventListener('DOMContentLoaded', async () => {
           return;
         }
         console.log(`Payment ${paymentIntent.status}: ${paymentIntent.id}`);
-          window.location.replace("/thank-you-early-access.html");
+         window.location.replace("/thank-you-early-access.html");
+          return;
       }
   
       console.log(`Payment ${paymentIntent.status}: ${paymentIntent.id}`);
+          window.location.replace("/thank-you-early-access.html");
+          return;
     });
   })
 })
