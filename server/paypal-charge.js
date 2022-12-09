03d98be73dee4ebe39c9eb4e3dc457e4372094ce
William-Boomer-Baker <boomer@boomerbaker.com>
2022-12-10 00:09:13 +0530

    test deploy

--- server/paypal-charge.js
+++ server/paypal-charge.js
@@ -1,6 +1,5 @@
-//paypal
 exports.handler = async (event, context) => {
-    try {
+
     if (event.httpMethod === "GET") {
         const createOrderPayload = {
             purchase_units: [
@@ -22,14 +21,4 @@ exports.handler = async (event, context) => {
             body: JSON.stringify({ message: "METHOD NOT ALLOWED" }),
         };
     }
-} catch (err) {
-
-    console.log("11");
-    console.log("Error: " + err);
-    return {
-      statusCode: 400,
-      body: err,
-    };
-
-  }
 };
